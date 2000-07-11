80b537e244c70bf25a4b7d6bb59f5bc0278c7e23
Kelly Fitz <kfitz@cerlsoundgroup.org>
2000-07-11 17:57:44 +0000

    Un-templatized DistributeEnergy, which should never have been templatized in the first place.

--- Loris/src/DistributeEnergy.C
+++ Loris/src/DistributeEnergy.C
@@ -17,6 +17,14 @@
 namespace Loris {
 #endif
 
+// ---------------------------------------------------------------------------
+//	constructor
+// ---------------------------------------------------------------------------
+//
+DistributeEnergy::DistributeEnergy( double distanceHz ) : 
+	_maxdist( distanceHz ) 
+{
+}
 
 // ---------------------------------------------------------------------------
 //	computeAlpha
@@ -28,8 +36,10 @@ namespace Loris {
 //	Could do lots of checking, but since computeAlpha is called only 
 //	by another member, just assume that below <= x <= above, and
 //	go for efficiency.
-double 
-DistributeEnergy::computeAlpha( double x, double below, double above ) const
+//
+//	static local function
+//
+static double computeAlpha( double x, double below, double above ) 
 {
 	return ( x - below ) / ( above - below );
 /*
@@ -55,9 +65,10 @@ DistributeEnergy::computeAlpha( double x, double below, double above ) const
 // ---------------------------------------------------------------------------
 //	Distribute energy to p as bandwidth over the time (lower,upper).
 //
-void
-DistributeEnergy::addNoiseEnergy( double energy, Partial & p, 
-								  double lower, double upper ) const
+//	static local function
+//
+static void addNoiseEnergy( double energy, Partial & p, 
+							double lower, double upper ) 
 {
 	if ( lower > upper ) {
 		std::swap( lower, upper );
@@ -66,14 +77,109 @@ DistributeEnergy::addNoiseEnergy( double energy, Partial & p,
 	//	loop over Breakpoints in p in the range (lower,upper):
 	for ( PartialIterator it = p.findPos( lower ); 
 		  it != p.end() && it.time() < upper;
-		  ++it ) {
+		  ++it ) 
+	{
 		//	don't transfer noise energy to 
 		//	zero-amplitude breakpoints, 
-		//	sounds bad?
+		//	sounds bad:
 		if ( it->amplitude() > 0. )
 			it->addNoise( energy );
 	}
 }
+
+// ---------------------------------------------------------------------------
+//	addNoiseEnergy
+// ---------------------------------------------------------------------------
+//
+void 
+DistributeEnergy::distribute( const Partial & p, 
+							  PartialList::iterator begin, 
+							  PartialList::iterator end ) const
+{
+	//	loop over Breakpoints in p:
+	PartialConstIterator envIter;
+	double tUpperBound = p.startTime();	// initialize:
+	for ( envIter = p.begin(); envIter != p.end(); ++envIter ) {
+		double time = envIter.time();
+		const Breakpoint & bp = * envIter;
+		
+		//	find nearest Partial in (begin,end) above
+		//	and below (in frequency) to bp at time:
+		PartialList::iterator above = end, below = end;
+		double freqAbove = 0., freqBelow = 0.;
+		for ( PartialList::iterator it = begin; it != end; ++it ) {
+			//	cannot distribute energy to a Partial
+			//	that does not exist at time:
+			if ( it->startTime() > time || it->endTime() < time ) {
+				continue;
+			}
+			
+			//	could also check to make sure that p isn't in
+			//	(begin, end), so as not to distribute p's
+			//	energy to itself:
+			if ( &(*it) == &p ) {
+				continue;
+			}
+
+			//	compare to other candidates:
+			double f = it->frequencyAt( time );
+			if ( f < bp.frequency() ) {
+				//	candidate below:
+				if ( below == end || f > freqBelow ) {
+					//	better than previous candidate below:
+					below = it;
+					freqBelow = f;
+				}
+			}
+			else {
+				//	candidate above:
+				if ( above == end || f < freqAbove ) {
+					//	better than previous candidate above:
+					above = it;
+					freqAbove = f;
+				}
+			}
+		}	//	end of loop over partials in (begin,end)
+		
+		//	compute time bounds for energy distribution:
+		//	(halfway between this breakpoint and its neighbors)
+		double tLowerBound = tUpperBound;
+		PartialConstIterator next( envIter );
+		if ( ++next != p.end() ) {
+			tUpperBound = ( next.time() + time ) * 0.5;
+		}
+		else {
+			tUpperBound = time;
+		}
+		
+		//	make sure the candidates aren't too far
+		if ( freqAbove - bp.frequency() > _maxdist ) {
+			above = end;
+		}
+		if ( bp.frequency() - freqBelow > _maxdist ) {
+			below = end;
+		} 
+		
+		
+		//	four cases: either, neither, or both could
+		//	be end (i.e. no candidate exists):
+		double e = bp.amplitude() * bp.amplitude();
+		if ( above != end && below != end ) {
+			double alpha = computeAlpha( bp.frequency(), freqBelow, freqAbove );
+			addNoiseEnergy( e * (1. - alpha), *below, tLowerBound, tUpperBound );
+			addNoiseEnergy( e * alpha, *above, tLowerBound, tUpperBound );
+		}
+		else if ( above != end ) {
+			addNoiseEnergy( e, *above, tLowerBound, tUpperBound );
+		}
+		else if ( below != end ) {
+			addNoiseEnergy( e, *below, tLowerBound, tUpperBound );
+		}
+		//	else no Partials in (begin,end) are eligible
+
+	}	//	end loop over partial envelope
+};
+
 
 #if !defined( NO_LORIS_NAMESPACE )
 }	//	end of namespace Loris
--- Loris/src/DistributeEnergy.h
+++ Loris/src/DistributeEnergy.h
@@ -1,5 +1,5 @@
-#ifndef __energy_distribution_header__
-#define __energy_distribution_header__
+#ifndef INCLUDE_DISTRIBUTEENERGY_H
+#define INCLUDE_DISTRIBUTEENERGY_H
 // ===========================================================================
 //	DistributeEnergy.h
 //	
@@ -9,15 +9,14 @@
 //
 // ===========================================================================
 #include "Partial.h"
-#include "Breakpoint.h"
-#include "notifier.h"
-#include "bark.h"
+//	can't get around including Partial.h until make a 
+//	class out of PartialList and its iterators that I can
+//	forward-declare
 
 #if !defined( NO_LORIS_NAMESPACE )
 //	begin namespace
 namespace Loris {
 #endif
-
 
 // ---------------------------------------------------------------------------
 //	class DistributeEnergy
@@ -30,118 +29,23 @@ namespace Loris {
 //
 class DistributeEnergy
 {
-	double _maxdist; 	//	the maximum frequency distance (in Hz) over
+	double _maxdist;//	the maximum frequency distance (in Hz) over
 					//	which energy will be redistributed; energy from
 					//	a discarded Partial will be distributed only
 					//	to Partials nearer in frequency than _maxdist
 public:
 	//	construction:
-	DistributeEnergy( double distanceHz ) : _maxdist( distanceHz ) {}
+	explicit DistributeEnergy( double distanceHz );
 	
-	//	distribute
-	template< class Iter >
-	void distribute( const Partial & p, Iter begin, Iter end ) const
-	{
-		//	loop over Breakpoints in p:
-		PartialConstIterator envIter;
-		double tUpperBound = p.startTime();	// initialize:
-		for ( envIter = p.begin(); envIter != p.end(); ++envIter ) {
-			double time = envIter.time();
-			const Breakpoint & bp = * envIter;
-			
-			//	find nearest Partial in (begin,end) above
-			//	and below (in frequency) to bp at time:
-			Iter above = end, below = end;
-			double freqAbove = 0., freqBelow = 0.;
-			for ( Iter it = begin; it != end; ++it ) {
-				//	cannot distribute energy to a Partial
-				//	that does not exist at time:
-				if ( it->startTime() > time || it->endTime() < time ) {
-					continue;
-				}
-				
-				//	could also check to make sure that p isn't in
-				//	(begin, end), so as not to distribute p's
-				//	energy to itself:
-				if ( &(*it) == &p ) {
-					continue;
-				}
-
-				//	compare to other candidates:
-				double f = it->frequencyAt( time );
-				if ( f < bp.frequency() ) {
-					//	candidate below:
-					if ( below == end || f > freqBelow ) {
-						//	better than previous candidate below:
-						below = it;
-						freqBelow = f;
-					}
-				}
-				else {
-					//	candidate above:
-					if ( above == end || f < freqAbove ) {
-						//	better than previous candidate above:
-						above = it;
-						freqAbove = f;
-					}
-				}
-			}	//	end of loop over partials in (begin,end)
-			
-			//	compute time bounds for energy distribution:
-			//	(halfway between this breakpoint and its neighbors)
-			double tLowerBound = tUpperBound;
-			PartialConstIterator next( envIter );
-			if ( ++next != p.end() ) {
-				tUpperBound = ( next.time() + time ) * 0.5;
-			}
-			else {
-				tUpperBound = time;
-			}
-			
-			//	make sure the candidates aren't too far
-			if ( freqAbove - bp.frequency() > _maxdist ) {
-				above = end;
-			}
-			if ( bp.frequency() - freqBelow > _maxdist ) {
-				below = end;
-			} 
-			
-			
-			//	four cases: either, neither, or both could
-			//	be end (i.e. no candidate exists):
-			double e = bp.amplitude() * bp.amplitude();
-			if ( above != end && below != end ) {
-				double alpha = computeAlpha( bp.frequency(), freqBelow, freqAbove );
-				addNoiseEnergy( e * (1. - alpha), *below, tLowerBound, tUpperBound );
-				addNoiseEnergy( e * alpha, *above, tLowerBound, tUpperBound );
-			}
-			else if ( above != end ) {
-				addNoiseEnergy( e, *above, tLowerBound, tUpperBound );
-			}
-			else if ( below != end ) {
-				addNoiseEnergy( e, *below, tLowerBound, tUpperBound );
-			}
-			//	else no Partials in (begin,end) are eligible
+	//	distribution:
+	void distribute( const Partial & p, PartialList::iterator begin, 
+										PartialList::iterator end ) const;
 	
-		}	//	end loop over partial envelope
-	};
-
-	template< class Iter >
-	void operator() ( const Partial & p, Iter begin, Iter end ) const
-	{ 
-		distribute( p, begin, end ); 
-	}
-	
-private:	
-//	-- helpers --
-	void addNoiseEnergy( double energy, Partial & p, double lower, double upper ) const;
-	double computeAlpha( double x, double below, double above ) const;
 	
 };	// end of class DistributeEnergy
-
 
 #if !defined( NO_LORIS_NAMESPACE )
 }	//	end of namespace Loris
 #endif
 
-#endif 	// ndef __energy_distribution_header__
+#endif 	// ndef INCLUDE_DISTRIBUTEENERGY_H
