09f91ed536c5c940f2d499e55bfd1e47ddb28d81
Kelly Fitz <kfitz@cerlsoundgroup.org>
2001-10-04 20:03:52 +0000

    New trials since Distiller was modified to perform sifting.

--- farm/cellos/cellos.py
+++ farm/cellos/cellos.py
@@ -18,7 +18,34 @@ as wide.
 
 F is for forte, MF is for mezzo-forte.
 
-Last updated: 23 May 2001 by Kelly Fitz
+notes from trial 1:
+
+cello154 
+	- very narrow (long) windows smoosh out the attack completely, 
+		can't use anything much narrower than 100 Hz
+	- some wierd frequency artifacts in the analyses using 146 Hz resolution
+	- only the 46 Hz resolution reconstruction matches the abruptness of the
+		original attack. Distilling this one still sounds mostly okay, but 
+		sifting it at 154 Hz ruins it completely. Other sifts (46 and 77) are
+		okay.
+
+cello69.F
+	- all sound fairly lousy
+	- windows narrower than 60 Hz are bad
+	- all sound very showery, except widest windows, which have other problems
+	- 65 Hz resolution is too wide
+	- 55.138 analysis sounds okay distilled, and maybe a little less showery 
+		sifted
+	- same could be said for 34.86 analysis, but none really sounds very good
+	
+cello69.MF
+	- all sound bad, showery
+	- narrow windows are, as usual, unusable
+	- 65 Hz resolution analyses are mostly yucky
+	- region width doesn't make any difference
+	- sifting and distilling don't seem to make these any worse
+
+Last updated: 29 May 2001 by Kelly Fitz
 """
 print __doc__
 
@@ -27,7 +54,7 @@ from trials import *
 
 # use this trial counter to skip over
 # eariler trials
-trial = 1
+trial = 2
 
 print "running trial number", trial, time.ctime(time.time())
 
@@ -55,3 +82,19 @@ if trial == 1:
 						harmonicSift( p2, f )
 						ofile = '%s.%i.%i.%ik.s%i.aiff'%(source[:-5], r, w, bw/1000, f)
 						synthesize( ofile, p2 )
+						
+if trial == 2:
+	source = 'cello154.F.aiff'
+	resolutions = (46, 77)
+	winds = (115, 154, 225, 300)
+	bw = 2000
+	for r in resolutions:
+		for w in winds:
+			p = analyze( source, r, w, bw )
+			ofile = '%s.%i.%i.aiff'%(source[:-5], r, w)
+			synthesize( ofile, p )
+			for f in (77, 154):
+				p2 = p.copy()
+				harmonicDistill( p2, f )
+				ofile = '%s.%i.%i.d%i.aiff'%(source[:-5], r, w, f)
+				synthesize( ofile, p2 )
