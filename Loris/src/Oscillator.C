b4c6ea945cd17874fba2f9b9cf5c264260357867
Kelly Fitz <kfitz@cerlsoundgroup.org>
2002-01-31 21:31:56 +0000

    Autobuild finally works under Darwin. Auto-files had to have special scaffolding for Darwin, and had to take gaussian_normal() out of line in Oscillator.C (so random.h is no longer needed) because of problems with -fno-common compilation (which is required under Darwin to build shared libraries).

--- Loris/src/Oscillator.C
+++ Loris/src/Oscillator.C
@@ -37,7 +37,7 @@
 
 #include <Oscillator.h>
 #include <Filter.h>
-#include "random.h"
+// #include "random.h"
 #include <Exception.h>
 #include <Notifier.h>
 #include <vector>
@@ -53,6 +53,105 @@
 //	begin namespace
 namespace Loris {
 #endif
+
+
+// ************* begin contents of random.h *******************
+
+//	trunc() is a problem. It's not in cmath, officially, though
+//	Metrowerks has it in there. SGI has it in math.h which is
+//	(erroneously!) included in g++ cmath, but trunc is not imported
+//	into std. For these two compilers, could just import std. But
+//	trnc doesn't seem to exist anywhere in Linux g++, so use std::modf().
+//	DON'T use integer conversion, because long int ins't as long 
+//	as double's mantissa, see below.
+static inline double trunc( double x ) { double y; std::modf(x, &y); return y; }
+
+// ---------------------------------------------------------------------------
+//		random number generator
+// ---------------------------------------------------------------------------
+//	Taken from "Random Number Generators: Good Ones Are Hard To Find," 
+//	Stephen Park and Keith Miller, Communications of the ACM, October 1988,
+//	vol. 31, Number 10.
+//
+//	This version will work as long as floating point values are represented
+//	with at least a 46 bit mantissa. The IEEE standard 64 bit floating point
+//	format (used on the PPC) has a 53 bit mantissa.
+//
+//	The correctness of the implementation can be checked by confirming that
+// 	after 10000 iterations, the seed, initialized to 1, is 1043618065.
+//	I have confirmed this. 
+//
+//	I have also confirmed that it still works (is correct after 10000 
+//	iterations) when I replace the divides with multiplies by oneOverM.
+//
+//	Returns a uniformly distributed random double on the range [0., 1.).
+//
+//	-kel 7 Nov 1997.
+//
+inline double
+uniform( void )
+{
+static const double a = 16807.L;
+static const double m = 2147483647.L;	// == LONG_MAX
+static const double oneOverM = 1.L / m;
+static double seed = 1.0L;
+
+	double temp = a * seed;
+	// seed = temp - m * trunc( temp / m );
+	seed = temp - m * trunc( temp * oneOverM );
+	// return seed / m;
+	return seed * oneOverM;
+}
+
+#pragma mark -
+#pragma mark === normal distribution ===
+
+// ---------------------------------------------------------------------------
+//	gaussian_normal
+// ---------------------------------------------------------------------------
+//	Approximate the normal distribution using the Box-Muller transformation.
+//	This is a better approximation and faster algorithm than the 12 u.v. sum.
+//	This is slightly different than the thing I got off the web, I (have to)
+//	assume (for now) that I knew what I was doing when I altered it.
+//
+/* inline */ double
+gaussian_normal( void )
+{
+static int iset = 0;	//	boolean really, 
+static double gset;
+
+	double r = 1., fac, v1, v2;
+	
+	if ( ! iset )
+	{
+		v1 = 2. * uniform() - 1.;
+		v2 = 2. * uniform() - 1.;
+		r = v1*v1 + v2*v2;
+		while( r >= 1. )
+		{
+			// v1 = 2. * uniform() - 1.;
+			v1 = v2;
+			v2 = 2. * uniform() - 1.;
+			r = v1*v1 + v2*v2;
+		}
+
+		fac = std::sqrt( -2. * std::log(r) / r );
+		gset = v1 * fac;
+		iset = 1;
+		return v2 * fac;
+	}
+	else
+	{
+		iset = 0;
+		return gset;
+	}
+}
+
+// ************* end contents of random.h *******************
+
+
+
+
 
 // ---------------------------------------------------------------------------
 //	protoype filter
