a3e125dc751bc4d9d48265aa9a0136d68d339e78
Kelly Fitz <kfitz@cerlsoundgroup.org>
2002-05-08 06:20:01 +0000

    My initial revisions to clean up file import.

--- Loris/csound/bweoscil.C
+++ Loris/csound/bweoscil.C
@@ -2,12 +2,13 @@
 #include "cs.h"
 #include "bwe2.h"
 #include "string.h"
+#include <Exception.h>
 #include <Oscillator.h>
 #include <Partial.h>
 #include <SdifFile.h>
-namespace foo {
-#include <math.h>  //have to add math.h this way, will get conflict otherwise
-}
+#include <exception>
+#include <string>
+#include <vector>
 
 using namespace Loris;
 using namespace std;
@@ -22,6 +23,15 @@ int n;
 float fadetime;
 }; 
 
+/* GET RID OF ALL THESE EXTERN C'S, THESE THINGS ARE ALL LOCAL */	
+
+/* I made a small step toward making this more efficient, by only
+	loading each SDIF file once. I am still unnecessarily making
+	copies of all the partials. There's lots of work to be done here.
+	-kel
+	7 May 2002
+ */
+ 
 //function gets the amplitude of a partial at a specific time
 extern "C"
 float getAmp(float time, Partial *part)
@@ -33,7 +43,7 @@ return part->amplitudeAt(time);
 extern "C"
 float getFreq(float time, Partial *part)
 {
-return (part->frequencyAt(time))*2*M_PI/esr;
+return (part->frequencyAt(time))*TWOPI/esr;
 }
 
 //function gets the bandwidth of a partial at a specific time
@@ -70,12 +80,70 @@ Oscillator *oscp = new Oscillator[n];
 return oscp;
 }
 
+#define MAXFILES (32)
+static SdifFile * files[MAXFILES];
+
+//	access a named SDIF file
+static int sdif_openfile(const std::string & filename)
+{
+	static std::vector< std::string > filenames(MAXFILES);
+	int i;
+    char *pname;
+    SdifFile *pfile = NULL;
+
+    for (i=0; i < MAXFILES; ++i) {
+      if (files[i]==NULL)	{				//	if we find an empty space, load 
+      										//	the file into that space
+        break;
+      }
+      else if ( filenames[i] == filename ) {//	if we find the one we're looking
+      										//	for, return its index
+      	fprintf(stderr, "** found SDIF file %s at index %d\n", filename.c_str(), i);
+      	return i;
+      }
+    }
+    
+    if (i==MAXFILES) 
+    {
+      fprintf(stderr, "\nERROR importing SDIF file: too many files open");
+      return -1;
+    }
+
+	try 
+	{
+    	files[i] = new SdifFile(filename);
+    	filenames[i] = filename;
+     	fprintf(stderr, "** importing SDIF file %s at index %d\n", filename.c_str(), i);
+    }
+    catch(Exception ex)
+    {
+      fprintf(stderr, "\nERROR importing SDIF file: %s", ex.what());
+      return -1;
+    }
+    catch(std::exception ex)
+    {
+      fprintf(stderr, "\nERROR importing SDIF file: %s", ex.what());
+      return -1;
+    }
+
+    return i;
+}
+
 //function creates all the partials specified by the input file
 extern "C"
 void createPartials(PRIVSTORE *bwestore)
 {
 int count = 0;
-SdifFile f(bwestore->strarg); //read in partials
+
+	int idx = sdif_openfile(bwestore->strarg);
+	if (idx < 0) {
+		bwestore->n = 0;
+		return;
+	}
+	
+	SdifFile & f = *(files[idx]);
+	//SdifFile f(bwestore->strarg); //read in partials
+
 bwestore->n = f.partials().size();  //the number of partials total
 bwestore->oscp = createOscillators(bwestore->n);  //create n oscillators
 list< Partial >::iterator iter;  //make a list of partials to iterate through
@@ -182,12 +250,26 @@ extern "C"
 void bwei(BWE *p)
 {
 	int i;
+
 	p->bwestore = (PRIVSTORE *)malloc(sizeof(PRIVSTORE));  //allocate storage structure
 	p->bwestore->fadetime = *(p->fadetime);  //store fadetime in bwestore
+	
+	/* import the SDIF file, this code copied from ugens8.c pvset() */
+	if (*p->ifilnam == sstrcod)                         /* if strg name given */
+      strcpy(p->bwestore->strarg, unquote(p->STRARG));           /*   use that         */
+    /* unclear what this does, not described in pvoc docs
+    else if ((long)*p->ifilnam <= strsmax && strsets != NULL && strsets[(long)*p->ifilnam])
+      strcpy(sdiffilnam, strsets[(long)*p->ifilnam]);
+     */
+    else 
+    	sprintf(p->bwestore->strarg,"loris.%d", (int)*p->ifilnam); /* else loris.filnum   */
+
+	
+	/* Corbin's code
 	if (*p->ifilnam == sstrcod)  //read in filename
 		strcpy(p->bwestore->strarg, unquote(p->STRARG)); 
-        createPartials(p->bwestore);
+     */    
+    createPartials(p->bwestore);
 	p->h.dopadr = trycleanup;  //set trycleanup as cleanup routine
 	p->bwestore->store = (double *)malloc(ksmps*sizeof(double));  //allocate memory for buffer to be used for generating samples into
 }
-
