33ddd0a52446e2cdb68942a0736e8d9fce210704
Kelly Fitz <kfitz@cerlsoundgroup.org>
2002-05-22 06:53:55 +0000

    Updated Sieve to be slightly more clever, and not need to sort the PartialList in place. Should now be able to remove the container argument to sift.

--- Loris/src/Sieve.C
+++ Loris/src/Sieve.C
@@ -37,29 +37,67 @@
 
 #include <Sieve.h>
 #include <Partial.h>
+#include <PartialList.h>
 #include <Breakpoint.h>
 #include <Exception.h>
 #include <PartialUtils.h>
 #include <Notifier.h>
 #include <algorithm>
 #include <list>
+#include <set>
 
 //	begin namespace
 namespace Loris {
 
+//	Definition of a comparitor for sorting a collection of pointers
+//	to Partials by label (increasing) and duration (decreasing), so
+//	that Partial ptrs are arranged by label, with the lowest labels
+//	first, and then with the longest Partials having each label
+//	before the shorter ones.
+struct SortPartialPtrs :
+	public std::binary_function< const Partial *, const Partial *, bool >
+{
+	bool operator()( const Partial * lhs, const Partial * rhs ) const 
+		{ 
+			return 	( lhs->label() != rhs->label() ) ?
+					( lhs->label() < rhs->label() ) :
+					( lhs->duration() > rhs->duration() );
+		}
+};
+
+//	Definition of a collection sorted by that comparitor.
+typedef std::set< Partial *, SortPartialPtrs > PartialPtrsSet;
+
+//	Definition of predicate for finding the end of a Patial *
+//	range having a common label.
+struct PartialPtrLabelNE :
+	public std::unary_function< const Partial *, bool >
+{
+	int label;
+	PartialPtrLabelNE( int l ) : label(l) {}
+
+	bool operator()( const Partial * p ) const
+		{ return p->label() != label; }
+};
+
+
 // ---------------------------------------------------------------------------
 //	sieve_aux STATIC
 // ---------------------------------------------------------------------------
-//	iterate over all other Partials with same labeling.
-//  if any other partial has time-overlap with this partial,
+//	Iterate over all other Partials with same labeling.
+//  If any other partial has time-overlap with this partial,
 //  and if this partial is not the longer of the two,
-//  then remove the label from this partial.
+//  then relabel this Partial with label 0.
 //
-//  return 1 if we remove the partial, else return zero.
+//	Overlap is defined by the minimum time gap between Partials
+//	(minGapTime), so Partials that have less then minGapTime
+//	between them are considered overlapping.
+//
+//  Return 1 if we relabel the partial, else return zero.
 //	
-static int sieve_aux( Partial &src, double minGapTime,
-				   	  std::list< Partial >::const_iterator start,
-				   	  std::list< Partial >::const_iterator end)
+static int sieve_aux( Partial & src, double minGapTime,
+				   	  std::list<Partial>::const_iterator start,
+				   	  std::list<Partial>::const_iterator end)
 {
 	std::list< Partial >::const_iterator it;
 	for ( it = start; it != end; ++it ) 
@@ -72,11 +110,7 @@ static int sieve_aux( Partial &src, double minGapTime,
 		//	(identity test: compare addresses)
 		//	(this is a sanity check, should not happen since
 		//	src should be at position end)
-		if ( &(*it) == &src )
-		{
-			debugger << "wierd: sieve_aux found src Partial in its iteration range" << endl;
-			continue;
-		}
+		Assert( &(*it) != &src );
 			
 		//  skip if no overlap:
 		if ( src.startTime() > it->endTime() + minGapTime ||
@@ -88,14 +122,10 @@ static int sieve_aux( Partial &src, double minGapTime,
 		//  are we longer duration?
 		//	(this should never be true, since the Partials
 		//	are sorted by duration)
-		if ( src.duration() > it->duration() ) 
-		{
-			debugger << "wierd: sieve_aux found src Partial longer than a Partial in the iteration range" << endl;
-			continue;
-		}
+		Assert( src.duration() <= it->duration() );
 		
 		//  we overlap wth something longer; remove us from this label.
-		/*
+#if Debug_Loris
 		debugger << "Partial starting " << src.startTime() << ", " 
 				 << src.frequencyAt( src.startTime() ) << " ending " 
 				 << src.endTime()  << ", " << src.frequencyAt( src.endTime() ) 
@@ -103,7 +133,8 @@ static int sieve_aux( Partial &src, double minGapTime,
 				 << it->startTime() << ", " << it->frequencyAt( it->startTime() )
 				 << " ending " << it->endTime() << ", " 
 				 << it->frequencyAt( it->endTime() ) << endl;
-		*/
+#endif
+
 		src.setLabel( 0 );
 		return 1;
 	}	//	end iteration over Partial range
@@ -111,10 +142,53 @@ static int sieve_aux( Partial &src, double minGapTime,
 	return 0;
 }
 
+template <typename Iter>	//	Iter must be the position of a Partial *
+Iter
+find_overlapping( Partial & p, double minGapTime, Iter start, Iter end)
+{
+	for ( Iter it = start; it != end; ++it ) 
+	{
+		//	skip if other partial is already sifted out.
+		if ( (*it)->label() == 0 )
+			continue;
+		
+		//	skip the source Partial:
+		//	(identity test: compare addresses)
+		//	(this is a sanity check, should not happen since
+		//	src should be at position end)
+		Assert( (*it) != &p );
+
+		//  test for overlap:
+		if ( p.startTime() < (*it)->endTime() + minGapTime &&
+			 p.endTime() + minGapTime > (*it)->startTime() )
+		{
+			//  Does the overlapping Partial have longer duration?
+			//	(this should never be true, since the Partials
+			//	are sorted by duration)
+			Assert( p.duration() <= (*it)->duration() );
+			
+#if Debug_Loris
+			debugger << "Partial starting " << p.startTime() << ", " 
+					 << p.begin().breakpoint().frequency() << " ending " 
+					 << p.endTime()  << ", " << (--p.end()).breakpoint().frequency() 
+					 << " zapped by overlapping Partial starting " 
+					 << (*it)->startTime() << ", " << (*it)->begin().breakpoint().frequency()
+					 << " ending " << (*it)->endTime() << ", " 
+					 << (--(*it)->end()).breakpoint().frequency()  << endl;
+#endif
+			return it;
+		}
+	}
+	
+	//	it is now the position of an overlapping Partial, or end:
+	return end;
+}
+
 // ---------------------------------------------------------------------------
 //	Sieve constructor
 // ---------------------------------------------------------------------------
-//	By default, use a gap time equal to 0.
+//	By default, use a gap time equal to 0, i.e. use the normal definition 
+//	of overlap to deermine which Partials get sifted out.
 //
 Sieve::Sieve( double minGapTime ) :
 	_minGapTime( minGapTime )
@@ -170,6 +244,13 @@ Sieve::sift( std::list<Partial> & container )
 //	copying (twice) would be too expensive for large data sets.
 //	So we are stuck requiring the list reference.
 //
+//	The only other alternative would be to make a collection
+//	(could be a set, which automatically sorts itself) of
+//	pointers to Partials. This wouldn't be as expensive, and
+//	but would require some new definitions, like the comparitor.
+//
+
+
 void 
 Sieve::sift( std::list<Partial> & container, 
 			 std::list< Partial >::iterator sift_begin, 
@@ -180,24 +261,32 @@ Sieve::sift( std::list<Partial> & container,
 	//	make a new temporary list that can be sorted and
 	//	distilled, since it isn't possible to sort a select
 	//	range of position in a list:
-	std::list<Partial> sift_list;
-	sift_list.splice( sift_list.begin(), container, sift_begin, sift_end );
+	//	(why am I making a temporary copy of this list whose
+	//	contents I am replacing anyway?)
+	//std::list<Partial> sift_list;
+	//sift_list.splice( sift_list.begin(), container, sift_begin, sift_end );
 	
 	//	sort the std::list< Partial > by length and label:
-	sift_list.sort( PartialUtils::duration_greater() );
-	sift_list.sort( PartialUtils::label_less() );
+	//sift_list.sort( PartialUtils::duration_greater() );
+	//sift_list.sort( PartialUtils::label_less() );
+	
+	PartialPtrsSet sift_set;
+	while ( sift_begin != sift_end )
+	{
+		sift_set.insert( &(*sift_begin) );
+		++sift_begin;
+	}
 	
 	// 	iterate over labels and sift each one:
-	std::list<Partial>::iterator lowerbound = sift_list.begin();
-	while ( lowerbound != sift_list.end() )
+	PartialPtrsSet::iterator lowerbound = sift_set.begin();
+	while ( lowerbound != sift_set.end() )
 	{
-		int label = lowerbound->label();
+		int label = (*lowerbound)->label();
 		
 		//	first the first element in l after sieve_begin
 		//	having a label not equal to 'label':
-		std::list<Partial>::iterator upperbound = 
-			std::find_if( lowerbound, sift_list.end(), 
-						  std::not1( std::bind2nd( PartialUtils::label_equals(), label ) ) );
+		PartialPtrsSet::iterator upperbound = 
+			std::find_if( lowerbound, sift_set.end(), PartialPtrLabelNE(label) );
 
 #ifdef Debug_Loris
 		//	don't want to compute this iterator distance unless debugging:
@@ -209,27 +298,33 @@ Sieve::sift( std::list<Partial> & container,
 		//	label is 0:
 		if ( label != 0 )
 		{
-			std::list< Partial >::iterator it;
+			PartialPtrsSet::iterator it;
 			for ( it = lowerbound; it != upperbound; ++it ) 
 			{
 				//	sieve_aux only needs to consider Partials on the
 				//	half-open range [lowerbound, it), because all 
 				//	Partials after it are shorter, thanks to the
-				//	sorting above:
-				zapped += sieve_aux( *it, _minGapTime, lowerbound, it );
+				//	sorting of the sift_set:
+				//zapped += sieve_aux( *it, _minGapTime, lowerbound, it );
+				
+				if( it != find_overlapping( **it, _minGapTime, lowerbound, it ) )
+				{
+					(*it)->setLabel(0);
+					++zapped;
+				}
 			} 
 		}
 		
-		//	advance Partial list iterator:
+		//	advance Partial set iterator:
 		lowerbound = upperbound;
 	}
 
 #ifdef Debug_Loris
-	debugger << "Sifted out (relabeled) " << zapped << " of " << sift_list.size() << "." << endl;
+	debugger << "Sifted out (relabeled) " << zapped << " of " << sift_set.size() << "." << endl;
 #endif
 
 	//	splice the Partials back into the original list:
-	container.splice( sift_end, sift_list );
+	//container.splice( sift_end, sift_list );
 
 }
 
