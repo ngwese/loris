25cfc7f7ff0e2dedd530ea21e598bf4baaa8fbca
Kelly Fitz <kfitz@cerlsoundgroup.org>
2003-04-15 05:48:13 +0000

    trying without BW association.

--- farm/funny/funny.py
+++ farm/funny/funny.py
@@ -49,17 +49,30 @@ notes from trial 4:
 	"pee"            902                 989
 	"puhl"          1173                1241
 	(end of "puhl") 1495                1521
+	
+	next, try without BW association (in Loris 1.0.3, set region width
+	to 0)
 
-Last updated: 10 June 2002 by Kelly Fitz
+notes from trial 5:
+	- raw syntheses sound very similar, hard to tell them apart. Need to look
+	in Kyma to see why they sound so different when I start distilling them.
+	- still seems like finer resolution is better, and probably the narrower
+	windows are better, but not by much, in the case of raw syntheses
+
+Last updated: 14 April 2003 by Kelly Fitz
 """
 print __doc__
 
 import loris, time
 from trials import *
+print """
+Using Loris version %s
+"""%loris.version()
+
 
 # use this trial counter to skip over
 # eariler trials
-trial = 4
+trial = 5
 
 print "running trial number", trial, time.ctime(time.time())
 
@@ -240,6 +253,90 @@ if trial == 4:
 			ofilebase = 'funny2.%i.%i'%(r, w)
 			synthesize( ofilebase + '.aiff', p )
 			loris.exportSdif( ofilebase + '.sdif', p )
+			for h in (1,2):
+				loris.channelize(p, ref, h)
+				# distilled version
+				pd = p.copy()
+				loris.distill( pd )
+				ofilebased = ofilebase + '.d%i'%h
+				loris.exportSdif( ofilebased + '.sdif', pd )
+				synthesize( ofilebased + '.aiff', pd )
+				pruneByLabel( pd, range(1,512) )
+				loris.exportSpc( ofilebased + '.s.spc', pd, 60, 0 ) 
+				loris.exportSpc( ofilebased + '.e.spc', pd, 60, 1 ) 
+		
+				# sifted version
+				ps = p.copy()
+				loris.sift( ps )
+				zeros = loris.extractLabeled( ps, 0 )
+				loris.distill( ps )
+				ofilebases = ofilebase + '.s%i'%h
+				loris.exportSdif( ofilebases + '.sdif', ps )
+				synthesize( ofilebases + '.aiff', ps )
+				pruneByLabel( ps, range(1,512) )
+				loris.exportSpc( ofilebases + '.s.spc', ps, 60, 0 ) 
+				loris.exportSpc( ofilebases + '.e.spc', ps, 60, 1 ) 
+
+if trial == 5:
+	source = 'funnyPeeple1.aiff'
+	pref = loris.importSpc('funnyPeeple1.fund.smooth.qharm')
+	ref = loris.createFreqReference( pref, 50, 1000 )
+	resolutions = ( 85, 155 )
+	widths = ( 300, 400 )
+	for r in resolutions:
+		for w in widths:
+			sfile = loris.AiffFile( source )
+			print 'analyzing %s (%s)'%(source, time.ctime(time.time()))
+			a = loris.Analyzer( r, w )
+			a.setBwRegionWidth( 0 ) # disable BW assoication
+			p = a.analyze( sfile.samples(), sfile.sampleRate() )
+			ofilebase = 'funny1.%i.%i'%(r, w)
+			pcollate = p.copy()
+			loris.distill( pcollate )
+			synthesize( ofilebase + '.raw.aiff', pcollate )
+			loris.exportSdif( ofilebase + '.sdif', p )
+			for h in (1,2):
+				loris.channelize(p, ref, h)
+				# distilled version
+				pd = p.copy()
+				loris.distill( pd )
+				ofilebased = ofilebase + '.d%i'%h
+				loris.exportSdif( ofilebased + '.sdif', pd )
+				synthesize( ofilebased + '.aiff', pd )
+				pruneByLabel( pd, range(1,512) )
+				loris.exportSpc( ofilebased + '.s.spc', pd, 60, 0 ) 
+				loris.exportSpc( ofilebased + '.e.spc', pd, 60, 1 ) 
+	
+				# sifted version
+				ps = p.copy()
+				loris.sift( ps )
+				zeros = loris.extractLabeled( ps, 0 )
+				loris.distill( ps )
+				ofilebases = ofilebase + '.s%i'%h
+				loris.exportSdif( ofilebases + '.sdif', ps )
+				synthesize( ofilebases + '.aiff', ps )
+				pruneByLabel( ps, range(1,512) )
+				loris.exportSpc( ofilebases + '.s.spc', ps, 60, 0 ) 
+				loris.exportSpc( ofilebases + '.e.spc', ps, 60, 1 ) 
+
+
+	source = 'funnyPeeple2.aiff'
+	pref = loris.importSpc('funnyPeeple2.fund.smooth.qharm')
+	ref = loris.createFreqReference( pref, 50, 1000 )
+	resolutions = ( 100, 170 )
+	widths = ( 400, 500 )
+	for r in resolutions:
+		for w in widths:
+			sfile = loris.AiffFile( source )
+			print 'analyzing %s (%s)'%(source, time.ctime(time.time()))
+			a = loris.Analyzer( r, w )
+			a.setBwRegionWidth( 0 ) # disable BW assoication
+			p = a.analyze( sfile.samples(), sfile.sampleRate() )
+			ofilebase = 'funny2.%i.%i'%(r, w)
+			pcollate = p.copy()
+			loris.distill( pcollate )
+			synthesize( ofilebase + '.raw.aiff', pcollate )
+			loris.exportSdif( ofilebase + 'raw.sdif', p )
 			for h in (1,2):
 				loris.channelize(p, ref, h)
 				# distilled version
