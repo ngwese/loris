33095b1de814276ba7cb7a80ff746667691989f4
Kelly Fitz <kfitz@cerlsoundgroup.org>
2003-06-04 23:22:58 +0000

    Added reverberant flute sound from dissertation experiments.

---
+++ farm/density/density.py
@@ -0,0 +1,62 @@
+"""
+density.py
+
+Python script for analyzing and reconstructing one of a variety 
+of sounds used to test the analysis/modification/synthesis routines 
+in Loris.
+
+This script pertains to the very reverberant flute solo excerpt 
+from Denisty 21.5 (Varese). There are eight notes, some long,
+some short, all very noisy and reverberant. This sound was used
+to test the frequency tracking analysis developed between releases
+1.0.2 and 1.0.3, but because of all the reverberation, frequency
+tracking is never going to give very good results, because it will
+always chop off the reverb decays. We once liked to use 150 Hz
+resolution, and a 200 Hz window, but more recently, it seems that
+maybe 100 and 300 were better parameters.
+
+A fundamental frequency contour extracted in Kyma is in
+density.fund.qharm (an spc file).
+
+Last updated: 4 June 2003 by Kelly Fitz
+"""
+
+print __doc__
+
+import loris, time
+print "using Loris version", loris.version()
+
+# use this trial counter to skip over
+# eariler trials
+trial = 1
+
+print "running trial number", trial, time.ctime(time.time())
+
+source = 'density.aiff'
+file = loris.AiffFile( source )
+samples = file.samples()
+rate = file.sampleRate()
+
+if trial == 1:
+	resolutions = ( 100, 150 )
+	widths = ( 200, 300 )
+	for r in resolutions:
+		for w in widths:
+			a = loris.Analyzer( r, w )
+			# turn off BW association for now
+			# a.setBwRegionWidth( 0 )
+			p = a.analyze( samples, rate )
+			# export raw
+			ofile = 'density.%i.%i.raw'%(r, w)
+			loris.distill( p )
+			# export
+			loris.exportAiff( ofile + '.aiff', loris.synthesize( p, rate ), rate, 1, 24 )
+			# prune before Spc export
+			iter = p.begin()
+			while not iter.equals( p.end() ):
+				next = iter.next()
+				if iter.partial().label() > 511:					
+					p.erase( iter )
+				iter = next
+			loris.exportSpc( ofile + '.s.spc', p, 60, 0 ) 
+			loris.exportSpc( ofile + '.e.spc', p, 60, 1 ) 
