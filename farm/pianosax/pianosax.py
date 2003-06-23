1ae2143a1d4a67a52d8549782426bf9546c492da
Kelly Fitz <kfitz@cerlsoundgroup.org>
2003-06-23 17:00:50 +0000

    Added a couple of trials.

--- farm/pianosax/pianosax.py
+++ farm/pianosax/pianosax.py
@@ -11,11 +11,28 @@ tracking analysis, developed after release 1.0.2, but didn't work long
 enough at it to make it sound good. We once liked to configure the
 analyzer with resolution and window width at 300Hz, but then needed
 to lower the frequency floor to about 90 Hz. For testing the frequency
-tracking analysis, I used resolution 150 Hz (or mayebe 90?), and 
+tracking analysis, I used resolution 150 Hz (or maybe 90?), and 
 window width 300 Hz.
 
 A fundamental frequency envelope traced in Kyma is in 
 pianosax.fund.qharm.
+
+trial 1:
+	- must have been using 90 Hz resolution (more importantly, 90 Hz
+	frequency floor) to get the thuds in the piano notes.
+	
+trial 2:
+	- setting frequency floor to 90 Hz is adequate, don't need resolution
+	that low
+	- tracking analyses are crunchy, try turning off BW association
+
+trial 3:
+	- sifting but leaving the zeros in still sounds crunchy, removing
+	the zeros sounds anemic and bubbly
+	- two partials per harmonic restores some of the sound, but also leaves
+	in some crunch or whoosh, can't figure out where it comes from.
+	- the crunch isn't actually noise, apparently, because it is still there
+	even if the noise energy in scaled to zero!
 
 Last updated: 4 June 2003 by Kelly Fitz
 """
@@ -27,7 +44,7 @@ print "using Loris version", loris.version()
 
 # use this trial counter to skip over
 # eariler trials
-trial = 1
+trial = 4
 
 print "running trial number", trial, time.ctime(time.time())
 
@@ -35,6 +52,8 @@ source = 'pianosax.aiff'
 file = loris.AiffFile( source )
 samples = file.samples()
 rate = file.sampleRate()
+pref = loris.importSpc('pianosax.fund.qharm')
+ref = loris.createFreqReference( pref, 50, 1000 )
 
 if trial == 1:
 	resolutions = ( 90, 150 )
@@ -59,3 +78,122 @@ if trial == 1:
 				iter = next
 			loris.exportSpc( ofile + '.s.spc', p, 60, 0 ) 
 			loris.exportSpc( ofile + '.e.spc', p, 60, 1 ) 
+
+if trial == 2:
+	resolutions = ( 90, 150 )
+	width = 300
+	for res in resolutions:
+		# do normal analysis
+		a = loris.Analyzer( res, width )
+		a.setFreqFloor( 90 )
+		p = a.analyze( samples, rate )
+		# export
+		ofile = 'pianosax.%i.%i.raw'%(res, width)
+		loris.distill( p )
+		# export
+		loris.exportAiff( ofile + '.aiff', loris.synthesize( p, rate ), rate, 1, 24 )
+		# prune before Spc export
+		iter = p.begin()
+		while not iter.equals( p.end() ):
+			next = iter.next()
+			if iter.partial().label() > 511:					
+				p.erase( iter )
+			iter = next
+		loris.exportSpc( ofile + '.s.spc', p, 60, 0 ) 
+		loris.exportSpc( ofile + '.e.spc', p, 60, 1 ) 
+		
+		# do tracking analysis
+		a = loris.Analyzer( res, width )
+		a.setFreqFloor( 90 )
+		p = a.analyze( samples, rate, ref )
+		# export
+		ofile = 'pianosax.%i.%i.trk'%(res, width)
+		loris.channelize(p, ref, 1)
+		loris.sift( p )
+		loris.distill( p )
+		# export
+		loris.exportAiff( ofile + '.aiff', loris.synthesize( p, rate ), rate, 1, 24 )
+		# prune before Spc export
+		iter = p.begin()
+		while not iter.equals( p.end() ):
+			next = iter.next()
+			if iter.partial().label() > 511:					
+				p.erase( iter )
+			iter = next
+		loris.exportSpc( ofile + '.s.spc', p, 60, 0 ) 
+		loris.exportSpc( ofile + '.e.spc', p, 60, 1 ) 
+		
+if trial == 3:
+	res = 150
+	width = 300
+	# do tracking analysis
+	a = loris.Analyzer( res, width )
+	a.setFreqFloor( 90 )
+	# turn off BW association for now
+	a.setBwRegionWidth( 0 )
+	p = a.analyze( samples, rate, ref )
+	# export
+	ofile = 'pianosax.%i.%i.trk'%(res, width)
+	loris.channelize(p, ref, 2)
+	loris.sift( p )
+	zeros = loris.extractLabeled( p, 0 )
+	loris.distill( p )
+	# scale noise to zero?
+	loris.scaleNoiseRatio( p, loris.BreakpointEnvelope(0) )
+	# export
+	loris.exportAiff( ofile + '.aiff', loris.synthesize( p, rate ), rate, 1, 24 )
+	# prune before Spc export
+	iter = p.begin()
+	while not iter.equals( p.end() ):
+		next = iter.next()
+		if iter.partial().label() > 511:					
+			p.erase( iter )
+		iter = next
+	loris.exportSpc( ofile + '.s.spc', p, 60, 0 ) 
+	loris.exportSpc( ofile + '.e.spc', p, 60, 1 ) 
+	
+if trial == 4:
+	res = 150
+	width = 300
+	# do tracking analysis with high amp floor
+	a = loris.Analyzer( res, width )
+	a.setFreqFloor( 90 )
+	a.setAmpFloor( -40 )
+	# turn off BW association for now
+	a.setBwRegionWidth( 0 )
+	p = a.analyze( samples, rate, ref )
+	
+	# use this analysis to create another reference env
+	loris.channelize(p, ref, 1)
+	loris.distill( p )
+	newref = loris.createFreqReference( p, 100, 500 )
+	
+	# let's look at these partials
+	loris.exportSpc( 'whoppee.s.spc', p, 60, 0 ) 
+	
+	# re-analyze with the new reference and
+	# a normal amp floor:
+	a.setAmpFloor( -90 )
+	p = a.analyze( samples, rate, newref )
+	
+	# export
+	ofile = 'pianosax.%i.%i.trk'%(res, width)
+	loris.channelize(p, newref, 2)
+	loris.sift( p )
+	# zeros = loris.extractLabeled( p, 0 )
+	loris.distill( p )
+	# scale noise to zero?
+	loris.scaleNoiseRatio( p, loris.BreakpointEnvelope(0) )
+	# export
+	loris.exportAiff( ofile + '.aiff', loris.synthesize( p, rate ), rate, 1, 24 )
+	# prune before Spc export
+	iter = p.begin()
+	while not iter.equals( p.end() ):
+		next = iter.next()
+		if iter.partial().label() > 511:					
+			p.erase( iter )
+		iter = next
+	loris.exportSpc( ofile + '.s.spc', p, 60, 0 ) 
+	loris.exportSpc( ofile + '.e.spc', p, 60, 1 ) 
+	
+
--- farm/shaku/shaku.py
+++ farm/shaku/shaku.py
@@ -13,11 +13,12 @@ The parameters we liked once were 80 Hz resolution and 300 Hz
 analysis window.
 
 trial1:
-	- sounds about the same as the best we did in CU, maybe even a little 
-	better?
+	- sounds okay, as before, the articulation is exaggerated for some reason
+	- why should this need 80 Hz resolution? The fundamental is over 400 Hz!
+trial 2:
 
 
-Last updated: 27 May 2003 by Kelly Fitz
+Last updated: 28 May 2003 by Kelly Fitz
 """
 print __doc__
 
@@ -27,7 +28,7 @@ print "using Loris version", loris.version()
 
 # use this trial counter to skip over
 # eariler trials
-trial = 1
+trial = 2
 print "running trial number", trial, time.ctime(time.time())
 
 
@@ -48,3 +49,67 @@ if trial == 1:
 	loris.exportAiff( ofile + '.aiff', loris.synthesize( p, rate ), rate, 1, 16 )
 	loris.exportSpc( ofile + '.s.spc', p, 60, 0 ) 
 	loris.exportSpc( ofile + '.e.spc', p, 60, 1 ) 
+
+if trial == 2:
+	resolutions = ( 80, 160, 240, 320 )
+	pref = loris.importSpc('shaku.fund.qharm')
+	ref = loris.createFreqReference( pref, 50, 1000 )
+	for res in resolutions:
+		for mlw in ( 300, 450, 600 ):
+			a = loris.Analyzer( res, mlw )
+			p = a.analyze( samples, rate )
+			d1 = p.copy()
+			d2 = p.copy()
+			
+			# export raw
+			loris.distill( p )
+			ofile = 'shaku.%i.%i.raw'%(res, mlw)
+			loris.exportAiff( ofile + '.aiff', loris.synthesize( p, rate ), rate, 1, 16 )
+			# prune before Spc export
+			iter = p.begin()
+			while not iter.equals( p.end() ):
+				if iter.partial().label() > 511:
+					next = iter.next()
+					p.erase( iter )
+					iter = next
+				else:
+					iter = iter.next()
+			loris.exportSpc( ofile + '.s.spc', p, 68, 0 ) 
+			loris.exportSpc( ofile + '.e.spc', p, 68, 1 ) 
+		
+			# distill one partial per harmonic
+			loris.channelize( d1, ref, 1 )
+			loris.distill( d1 )
+			# export
+			ofile = 'shaku.%i.%i.d1'%(res, mlw)
+			loris.exportAiff( ofile + '.aiff', loris.synthesize( d1, rate ), rate, 1, 16 )
+			# prune before Spc export
+			iter = d1.begin()
+			while not iter.equals( d1.end() ):
+				if iter.partial().label() > 511:
+					next = iter.next()
+					d1.erase( iter )
+					iter = next
+				else:
+					iter = iter.next()
+			loris.exportSpc( ofile + '.s.spc', d1, 68, 0 ) 
+			loris.exportSpc( ofile + '.e.spc', d1, 68, 1 ) 
+		
+			# distill two partials per harmonic
+			loris.channelize( d2, ref, 2 )
+			loris.distill( d2 )
+			# export
+			ofile = 'shaku.%i.%i.d2'%(res, mlw)
+			loris.exportAiff( ofile + '.aiff', loris.synthesize( d2, rate ), rate, 1, 16 )
+			# prune before Spc export
+			iter = d2.begin()
+			while not iter.equals( d2.end() ):
+				if iter.partial().label() > 511:
+					next = iter.next()
+					d2.erase( iter )
+					iter = next
+				else:
+					iter = iter.next()
+			loris.exportSpc( ofile + '.s.spc', d2, 68, 0 ) 
+			loris.exportSpc( ofile + '.e.spc', d2, 68, 1 ) 
+		
