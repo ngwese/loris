d9608a11ec5a8b112620f4c1e5dfae4ce1dc8dc4
Kelly Fitz <kfitz@cerlsoundgroup.org>
2004-07-02 23:54:56 +0000

    Added new analyzer utility program called loris_analyze. Still under development.

---
+++ Loris/utils/loris_analyze.C
@@ -0,0 +1,519 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include <AiffFile.h>
+#include <Analyzer.h>
+#include <Distiller.h>
+#include <SdifFile.h>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class Command; 
+typedef std::stack< std::string > Arguments;
+typedef std::map< std::string, Command * > CmdDictionary;
+
+// ----------------------------------------------------------------
+//	Abstract Command class
+// ----------------------------------------------------------------
+
+class Command
+{
+public:
+	virtual ~Command( void ) {};
+	virtual void execute( Arguments & args ) const = 0;
+};
+
+// ----------------------------------------------------------------
+//	global program state
+// ----------------------------------------------------------------
+string gInFileName, gOutFileName = "partials.sdif", gTestFileName;
+Loris::Analyzer * gAnalyzer = 0;
+bool gCollate = false;
+
+// ----------------------------------------------------------------
+//	argument parsing
+// ----------------------------------------------------------------
+static bool argIsNumber( const string & s, double * ret= 0 )
+{
+	const char * cstr = s.c_str();
+	char * endptr = 0;
+	double x = std::strtod( cstr, &endptr );
+	if ( endptr == cstr )
+	{
+		return false;
+	}
+	else
+	{
+		if ( ret != 0 )
+		{
+			*ret = x;
+		}
+		return true;
+	}
+}
+
+static bool argIsFlag( const string & s )
+{
+	return s[0] == '-' && ! argIsNumber(s);
+}
+
+static string lowercaseArg( string s )
+{
+	for ( string::iterator it = s.begin(); it != s.end(); ++it )
+	{
+		*it = std::tolower(*it);
+	}
+	return s;
+}
+
+// ----------------------------------------------------------------
+//	Concrete Commands
+// ----------------------------------------------------------------
+
+class InfileCommand : public Command
+{
+public:
+	//	set the global input filename
+	void execute( Arguments & args ) const 
+	{
+		//	requires a string specifying the filename
+		if ( args.empty() || argIsFlag( args.top() ) )
+		{
+			throw std::invalid_argument("input file specification "
+									   "requires a filename");
+		}
+		
+		gInFileName = args.top();
+		cout << "* using input (AIFF) file: " << gInFileName << endl;
+
+		args.pop();
+	}
+};
+
+class OutfileCommand : public Command
+{
+public:
+	//	set the global output filename
+	void execute( Arguments & args ) const 
+	{
+		//	requires a string specifying the filename
+		if ( args.empty() || argIsFlag( args.top() ) )
+		{
+			throw std::invalid_argument("output file specification "
+									   "requires a filename");
+		}
+		
+		gOutFileName = args.top();
+		cout << "* using output (SDIF) file: " << gOutFileName << endl;
+
+		args.pop();
+	}
+};
+
+class TestfileCommand : public Command
+{
+public:
+	//	set the global filename for a test render
+	void execute( Arguments & args ) const 
+	{
+		//	accepts a string specifying the filename, 
+		//	default is test.aiff
+		if ( args.empty() || argIsFlag( args.top() ) )
+		{
+			gTestFileName = "test.aiff";
+		}
+		else
+		{
+			gTestFileName = args.top();
+			args.pop();
+		}
+		
+		cout << "* rendering to (AIFF) file: " << gTestFileName << endl;
+	}
+};
+
+class CollateCommand : public Command
+{
+public:
+	//	set the global flag indicating that the
+	//	Partials should be collated before export
+	void execute( Arguments &  ) const 
+	{
+		gCollate = true;
+		cout << "* will collate partials" << endl;
+	}
+};
+
+class SetHopTimeCommand : public Command
+{
+public:
+	//	set the hopTime parameter of the global
+	//	Loris Analyzer
+	void execute( Arguments & args ) const 
+	{
+		//	requires a numeric parameter
+		double x;
+		if ( args.empty() || !argIsNumber( args.top(), &x ) )
+		{
+			throw std::invalid_argument("hop time specification "
+									   "requires a number");
+		}
+		
+		if ( x <= 0 )
+		{
+			throw std::invalid_argument("hop time specification "
+									   "must be positive");
+		}
+		
+		gAnalyzer->setHopTime( x );
+		cout << "* setting analysis hop time to: ";
+		cout << gAnalyzer->hopTime() << " s" << endl;
+
+		args.pop();
+	}
+};
+	
+class SetCropTimeCommand : public Command
+{
+public:
+	//	set the cropTime parameter of the global
+	//	Loris Analyzer
+	void execute( Arguments & args ) const 
+	{
+		//	requires a numeric parameter
+		double x;
+		if ( args.empty() || !argIsNumber( args.top(), &x ) )
+		{
+			throw std::invalid_argument("crop time specification "
+									   "requires a number");
+		}
+		
+		if ( x <= 0 )
+		{
+			throw std::invalid_argument("crop time specification "
+									  "must be positive");
+		}
+
+		gAnalyzer->setCropTime( x );
+		cout << "* setting analysis crop time to: ";
+		cout << gAnalyzer->cropTime() << " s" << endl;
+
+		args.pop();
+	}
+};
+	
+class SetRegionWidthCommand : public Command
+{
+public:
+	//	set the BWRegionWidth parameter of the global
+	//	Loris Analyzer
+	void execute( Arguments & args ) const 
+	{
+		//	requires a numeric parameter
+		double x;
+		if ( args.empty() || !argIsNumber( args.top(), &x ) )
+		{
+			throw std::invalid_argument("BW region width specification "
+									   "requires a number");
+		}
+		
+		if ( x < 0 )
+		{
+			throw std::invalid_argument("BW region width specification "
+									  "must be non-negative");
+		}
+		
+		gAnalyzer->setBwRegionWidth( x );
+		cout << "* setting analysis BW region width to: ";
+		cout << gAnalyzer->bwRegionWidth() << " Hz" << endl;
+
+		args.pop();
+	}
+};
+	
+class SetDriftCommand : public Command
+{
+public:
+	//	set the frequencyDrift parameter of the global
+	//	Loris Analyzer
+	void execute( Arguments & args ) const 
+	{
+		//	requires a numeric parameter
+		double x;
+		if ( args.empty() || !argIsNumber( args.top(), &x ) )
+		{
+			throw std::invalid_argument("frequency drift specification "
+									   "requires a number");
+		}
+		
+		if ( x <= 0 )
+		{
+			throw std::invalid_argument("frequency drift specification "
+									  "must be positive");
+		}
+		
+		gAnalyzer->setFreqDrift( x );
+		cout << "* setting analysis frequency drift to: ";
+		cout << gAnalyzer->freqDrift() << " Hz" << endl;
+
+		args.pop();
+	}
+};
+	
+class SetAmpFloorCommand : public Command
+{
+public:
+	//	set the ampFloor parameter of the global
+	//	Loris Analyzer
+	void execute( Arguments & args ) const 
+	{
+		//	requires a numeric parameter
+		double x;
+		if ( args.empty() || !argIsNumber( args.top(), &x ) )
+		{
+			throw std::invalid_argument("amplitude floor specification "
+									   "requires a number");
+		}
+		
+		if ( x >= 0 )
+		{
+			throw std::invalid_argument("amplitude floor specification "
+									  "must be negative");
+		}
+		
+		gAnalyzer->setAmpFloor( x );
+		cout << "* setting analysis amplitude floor to: ";
+		cout << gAnalyzer->ampFloor() << " dB" << endl;
+
+		args.pop();
+	}
+};
+	
+class SetFreqFloorCommand : public Command
+{
+public:
+	//	set the freqFloor parameter of the global
+	//	Loris Analyzer
+	void execute( Arguments & args ) const 
+	{
+		//	requires a numeric parameter
+		double x;
+		if ( args.empty() || !argIsNumber( args.top(), &x ) )
+		{
+			throw std::invalid_argument("frequency floor specification "
+									   "requires a number");
+		}
+		
+		if ( x <= 0 )
+		{
+			throw std::invalid_argument("frequency floor specification "
+									  "must be positive");
+		}
+		
+		gAnalyzer->setFreqFloor( x );
+		cout << "* setting analysis frequency floor to: ";
+		cout << gAnalyzer->freqFloor() << " Hz" << endl;
+
+		args.pop();
+	}
+};
+	
+class SetAttenuationCommand : public Command
+{
+public:
+	//	set the sidelobe attenuation parameter of the global
+	//	Loris Analyzer
+	void execute( Arguments & args ) const 
+	{
+		//	requires a numeric parameter
+		double x;
+		if ( args.empty() || !argIsNumber( args.top(), &x ) )
+		{
+			throw std::invalid_argument("sidelobe attenuation specification "
+									   "requires a number");
+		}
+		
+		if ( x <= 0 )
+		{
+			throw std::invalid_argument("sidelobe attenuation specification "
+									  "must be positive");
+		}
+		
+		gAnalyzer->setSidelobeLevel( x );
+		cout << "* setting analysis sidelobe attenuation to: "; 
+		cout << gAnalyzer->sidelobeLevel() << " dB" << endl;
+
+		args.pop();
+	}
+};
+	
+
+// ----------------------------------------------------------------
+//	parseArguments
+// ----------------------------------------------------------------
+//	Process each command line argument in sequence, and execute
+//	the command associated with each one. Throw 
+//	invalid_argument if an unrecognized argument is encountered,
+//	and allow any exceptions generated by executing the command
+//	to propogate.
+//
+static void parseArguments( Arguments & args, const CmdDictionary & commands )
+{
+	if ( args.size() < 2 )
+	{
+		throw std::invalid_argument("insufficient arguments");
+	}
+	
+	//	the first argument is the program name
+	cout << "running " << args.top() << endl;
+	args.pop();
+	
+	//	the second argument specifies the resolution at
+	//	which to configure the Analyzer
+	double resolution;
+	if ( ! argIsNumber( args.top(), &resolution ) )
+	{
+		throw std::invalid_argument("first argument must be analyzer resolution");
+	}
+	args.pop();
+	
+	//	if the next argument is not an flag and it is a
+	//	number, then it is the window width at which to 
+	//	configure the Analyzer. If not specified, the
+	//	window width is twice the resolution.
+	//
+	//	if the next argument is not a flag and is not a 
+	//	number, then it is the input filename. If not specified
+	//	here, then either the '-i' flag will specify it, or
+	//	samples will be read from standard input:
+	double winwidth = 2*resolution;
+	if ( !args.empty() && argIsNumber( args.top(), &winwidth ) )
+	{
+		args.pop();
+	}
+	cout << "* configuring Loris Analyzer with resolution " << resolution;
+	cout << " and window width " << winwidth << endl;
+	gAnalyzer = new Loris::Analyzer( resolution, winwidth );
+	
+	//	if the next argument is not a flag and is not a 
+	//	number, then it is the input filename. If not specified
+	//	here, then either the '-i' flag will specify it, or
+	//	samples will be read from standard input:
+	if ( !args.empty() && !argIsFlag( args.top() ) && !argIsNumber( args.top() ) )
+	{
+		InfileCommand cmd;
+		cmd.execute( args );
+	}
+	
+	//	invariant: there are more command line arguments
+	//	to be processed
+	while ( !args.empty() )
+	{
+		string cmd = lowercaseArg( args.top() );
+		CmdDictionary::const_iterator it = commands.find( cmd );
+		if ( it == commands.end() )
+		{
+			throw std::invalid_argument( "unrecognized argument " + args.top() );
+		}
+		
+		args.pop();
+		it->second->execute( args );	
+	}
+}
+
+// ----------------------------------------------------------------
+//	main
+// ----------------------------------------------------------------
+
+int main( int argc, char * argv[] )
+{
+	//	build a dictionary of commands
+	CmdDictionary commands;
+	commands["-i"] = commands["-in"] = commands["-ifile"] = commands["-infile"] = 
+		new InfileCommand();
+	commands["-o"] = commands["-out"] = commands["-ofile"] = commands["-outfile"] =
+		new OutfileCommand();
+	commands["-render"] = commands["-synth"] = new TestfileCommand();
+	commands["-collate"] = new CollateCommand();
+	commands["-hop"] = commands["-hoptime"] = new SetHopTimeCommand();
+	commands["-crop"] = commands["-croptime"] = new SetCropTimeCommand();
+	commands["-bw"] = commands["-bwregionwidth"] = new SetRegionWidthCommand();
+	commands["-drift"] = commands["-freqdrift"] = new SetDriftCommand();
+	commands["-ampfloor"] = new SetAmpFloorCommand();
+	commands["-freqfloor"] = new SetFreqFloorCommand();
+	commands["-sidelobes"] = commands["-attenutation"] = commands["-sidelobelevel"] =
+		new SetAttenuationCommand();
+	
+	// 	build an argument stack, pushing the arguments
+	//	in reverse order.
+	//	invariant: argc command line arguments remain to be enqueued
+	Arguments args;
+	while (argc-- > 0)
+	{
+		args.push( argv[argc] );
+	}
+	
+	try
+	{
+		parseArguments( args, commands );
+	}
+	catch ( std::logic_error & ex )
+	{
+		cout << "Error parsing arguments: \n\t" << ex.what() << endl;
+		cout << "usage: " << argv[0] << " resolution ";
+		cout << "[windowWidth] [infilename.aiff] [flags]" << endl;
+		return 1;
+	}
+	
+	//	run the analysis
+	try
+	{
+		cout << "* reading samples from " << gInFileName << endl;
+		Loris::AiffFile infile( gInFileName );
+		Loris::AiffFile::samples_type samples = infile.samples();
+		double rate = infile.sampleRate();
+		
+		cout << "* performing analysis" << endl;
+		gAnalyzer->analyze( samples, rate );
+		cout << "* analysis complete" << endl;	
+		
+		if ( gCollate )
+		{
+			cout << "* collating " << gAnalyzer->partials().size();
+			cout << " partials" << endl;
+			Loris::Distiller dist;
+			dist.distill( gAnalyzer->partials() );
+		}
+			
+		cout << "* exporting " << gAnalyzer->partials().size(); 
+		cout << " partials to " << gOutFileName << endl;
+		Loris::SdifFile outfile( gAnalyzer->partials().begin(), 
+								gAnalyzer->partials().end() );
+		outfile.write( gOutFileName );
+		
+		if ( ! gTestFileName.empty() )
+		{
+			cout << "* exporting rendered partials to " << gTestFileName << endl;
+			Loris::AiffFile testfile( gAnalyzer->partials().begin(), 
+									gAnalyzer->partials().end(), rate );
+			testfile.write( gTestFileName );
+		}
+		
+		cout << "* Done." << endl;
+	}
+	catch ( std::exception & ex )
+	{
+		cout << "Error running analysis: " << ex.what() << endl;
+		return 1;
+	}
+	
+	return 0;
+} 
