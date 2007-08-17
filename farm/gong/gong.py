8905b93f070037cfb72a503b67c9b6c4fc7ed50d
Kelly Fitz <kfitz@cerlsoundgroup.org>
2007-08-17 05:55:10 +0000

    updated and verified with Loris 1.5 (beta)

--- farm/gong/gong.py
+++ farm/gong/gong.py
@@ -1,79 +1,73 @@
 #!/usr/bin/python
-
 """
 gong.py
 
-Python script for analyzing and reconstructing one of a variety 
-of sounds used to test the analysis/modification/synthesis routines 
-in Loris.
-
-This script pertains to the orchestral gong sample used in the
+Analyze and reconstruct the orchestral gong sample used in the
 dissertation experiments. It is taken from the MUMS disks, and
 has a very noisy sounding onset, but also some sustained partials
 giving the gong its pitch, and a strong thud in the attack that
-has been hard to reproduce.
+is hard to reproduce.
 
-The parameters we once liked for this are resolution 25 and
-window width 75 Hz.
+25 and 30 Hz resolution work well enough with window width 75 Hz.
 
-notes from trial 1:
-	- 50 Hz has artifacts
-	- 200 Hz window is too wide
-	- 20,25, 30 res with 75 or 100 window are hard to distinguish
-	- distillations are very crunchy, esp at 100 and 150 Hz, even 
-	distillation at the resolution frequency introduces garbage.
-	
-notes from trial 2: 
-	all of these raw reconstructions are pretty much indistinguishible
+Distillation doesn't sound so great.
 
-Last updated: 27 July 2005 by Kelly Fitz
+Last updated: 16 Aug 2007 by Kelly Fitz
 """
-print __doc__
 
-import loris, time
+import loris, time, os
 
-print """
-Using Loris version %s
-"""%loris.version()
+orate = 44100
 
-# use this trial counter to skip over
-# eariler trials
-trial = 2
+tag = ''
 
-print "running trial number", trial, time.ctime(time.time())
+stuff = {}
 
-source = 'gong.aiff'
+# ----------------------------------------------------------------------------
 
-file = loris.AiffFile( source )
-samples = file.samples()
-rate = file.sampleRate()
+def doGong( res, exportDir = '' ):
 
-# if trial == 1:
-# 	resolutions = (20,25,30,50)
-# 	widths = ( 75,100,200 )
-# 	funds = ( 50,100,150 )
-# 	for r in resolutions:
-# 		for w in widths:
-# 			p = analyze( source, r, w )
-# 			ofile = 'gong.%i.%i.aiff'%(r, w)
-# 			synthesize( ofile, p )
-# 			for f in funds:
-# 				p2 = p.copy()
-# 				harmonicDistill( p2, f )
-# 				ofile = 'gong.%i.%i.d%i.aiff'%(r, w, f)
-# 				synthesize( ofile, p2 )
+	name = 'gong'
+	f = loris.AiffFile( name + '.aiff' )
+	
+	print 'analyzing %s (%s)'%(name, time.ctime(time.time()))
+	print 'using resolution %4f Hz'%(res)
+	anal = loris.Analyzer( res, 75 )
+	anal.setBwRegionWidth( 0 ) 	# or anal.storeNoBandwidth()
+	p = anal.analyze( f.samples(), f.sampleRate() )
 
-if trial == 2:
-	resolutions = ( 20,30 )
-	widths = ( 75,100 )
-	for r in resolutions:
-		for w in widths:
-			a = loris.Analyzer( r, w )
-			p = a.analyze( samples, rate )
-			ofile = 'gong.%i.%i.raw'%(r, w)
-			# collate
-			loris.collate( p )
-			# export
-			loris.exportAiff( ofile + '.aiff', loris.synthesize( p, rate ), rate, 16 )
-			loris.exportSpc( ofile + '.s.spc', p, 60, 0 ) 
-			loris.exportSpc( ofile + '.e.spc', p, 60, 1 ) 
+	name = '%s%d'%( name, res )
+	if exportDir:
+	
+		print 'synthesizing %i raw partials (%s)'%(p.size(), time.ctime(time.time()))
+		out_sfile = loris.AiffFile( p, orate )
+		
+		opath = os.path.join( exportDir, name + tag + '.aiff' ) 
+		print 'writing %s (%s)'%(opath, time.ctime(time.time()))
+		out_sfile.setMarkers( f.markers() )
+		out_sfile.write( opath )
+		
+		opath = os.path.join( exportDir, name + tag + '.sdif' )
+		print 'writing %s (%s)'%(opath, time.ctime(time.time()))
+		out_pfile = loris.SdifFile( p )
+		out_pfile.setMarkers( f.markers() )
+		out_pfile.write( opath )
+
+	stuff[ name ] = ( p, anal )
+		
+		
+# ----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+	print __doc__
+
+	print 'Using Loris version %s'%( loris.version() )
+
+	import sys
+	odir = os.curdir
+	if len( sys.argv ) > 1:
+		tag = '.' + sys.argv[1]
+		
+	doGong( 20, odir )
+	doGong( 30, odir )
+		
