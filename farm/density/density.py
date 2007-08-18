cb90fdd97a264b44ec24a3131d5bba2558a4654a
Kelly Fitz <kfitz@cerlsoundgroup.org>
2007-08-18 17:00:45 +0000

    updated and verified with Loris 1.5 (beta 13)

--- farm/density/density.py
+++ farm/density/density.py
@@ -1,62 +1,76 @@
+#!/usr/bin/python
+
 """
 density.py
 
-Python script for analyzing and reconstructing one of a variety 
-of sounds used to test the analysis/modification/synthesis routines 
-in Loris.
+Analyze and reconstruct a very reverberant flute solo excerpt from
+Denisty 21.5 (Varese). There are eight notes, some long, some short, all
+very noisy and reverberant. Because of all the reverberation, frequency
+tracking and channelization is never going to give very good results,
+because it will always chop off the reverb decays. 
 
-This script pertains to the very reverberant flute solo excerpt 
-from Denisty 21.5 (Varese). There are eight notes, some long,
-some short, all very noisy and reverberant. This sound was used
-to test the frequency tracking analysis developed between releases
-1.0.2 and 1.0.3, but because of all the reverberation, frequency
-tracking is never going to give very good results, because it will
-always chop off the reverb decays. We once liked to use 150 Hz
-resolution, and a 200 Hz window, but more recently, it seems that
-maybe 100 and 300 were better parameters.
-
-A fundamental frequency contour extracted in Kyma is in
-density.fund.qharm (an spc file).
-
-Last updated: 26 July 05 by Kelly Fitz
+Last updated: 18 Aug 07 by Kelly Fitz
 """
 
-print __doc__
+import loris, time, os
 
-import loris, time
-print "using Loris version", loris.version()
+orate = 44100
 
-# use this trial counter to skip over
-# eariler trials
-trial = 1
+tag = ''
 
-print "running trial number", trial, time.ctime(time.time())
+stuff = {}
 
-source = 'density.aiff'
-file = loris.AiffFile( source )
-samples = file.samples()
-rate = file.sampleRate()
+# ----------------------------------------------------------------------------
 
-if trial == 1:
-	resolutions = ( 100, 150 )
-	widths = ( 200, 300 )
-	for r in resolutions:
-		for w in widths:
-			a = loris.Analyzer( r, w )
-			# turn off BW association for now
-			# a.setBwRegionWidth( 0 )
-			p = a.analyze( samples, rate )
-			# export raw
-			ofile = 'density.%i.%i.raw'%(r, w)
-			loris.collate( p )
-			# export
-			fout = loris.AiffFile( p, rate )
-			fout.write( ofile + '.aiff' )
-			# remove any Partials labeled greater than 512
-			pruneMe = -1
-			for part in p:
-				if part.label() > 512:
-					part.setLabel( pruneMe )
-			loris.removeLabeled( p, pruneMe )
-			loris.exportSpc( ofile + '.s.spc', p, 60, 0 ) 
-			loris.exportSpc( ofile + '.e.spc', p, 60, 1 ) 
+def doDensity( exportDir = '' ):
+
+	name = 'density'
+	f = loris.AiffFile( name + '.aiff' )
+
+	print 'analyzing %s (%s)'%(name, time.ctime(time.time()))
+	
+	# also 150,300 works pretty well, changing the 
+	# window to 200 Hz starts to smear away the attacks
+	anal = loris.Analyzer( 100, 300 )
+	
+	# I wonder how well this fundamental tracker
+	# works on a tough sound like this one
+	anal.buildFundamentalEnv( 250, 850 )
+	
+	p = anal.analyze( f.samples(), f.sampleRate() )
+
+	if exportDir:
+
+		print 'synthesizing %i raw partials (%s)'%(p.size(), time.ctime(time.time()))
+		out_sfile = loris.AiffFile( p, orate )
+		
+		opath = os.path.join( exportDir, name + tag + '.raw.aiff' ) 
+		print 'writing %s (%s)'%(opath, time.ctime(time.time()))
+		out_sfile.setMarkers( f.markers() )
+		out_sfile.write( opath )
+		
+		opath = os.path.join( exportDir, name + tag + '.raw.sdif' )
+		print 'writing %s (%s)'%(opath, time.ctime(time.time()))
+		out_pfile = loris.SdifFile( p )
+		out_pfile.setMarkers( f.markers() )
+		out_pfile.write( opath )
+
+	stuff[ name ] = ( p, anal )
+
+	print 'Done. (%s)'%(time.ctime(time.time()))
+	
+	
+	
+# ----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+	print __doc__
+
+	print 'Using Loris version %s'%( loris.version() )
+
+	import sys
+	odir = os.curdir
+	if len( sys.argv ) > 1:
+		tag = '.' + sys.argv[1]
+		
+	doDensity( odir )	
