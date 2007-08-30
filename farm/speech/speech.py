48e68be3019e260217fb0a3151d0ce3a3ef14581
Kelly Fitz <kfitz@cerlsoundgroup.org>
2007-08-30 06:08:35 +0000

    Integrated nonsense and moses scripts into the speech script.

--- farm/speech/speech.py
+++ farm/speech/speech.py
@@ -8,19 +8,11 @@ Analyze and reconstruct a variety of speech sounds, including:
 	  the Camel analyzer
 	- the "funny people" samples we have used for testing 
 	  Loris for many years
+	- Kenneth Branagh speaking in a low, slow half-whisper, 
+	  speaking the name "Moses"
 	
-The fried vocal ("nonsense") definitely gives better harmonic tracking when 
-reanalyzed using the tracking analysis and the fundamental extracted in the 
-first analysis, BUT the reconstruction is a little bit worse in that case. 
 
-Two partials per harmonic are always necessary for decent
-sound, and the sound doesn't improve with the improved tracking.
-One partial per harmonic is completely unlistenable.
-
-The "funny people" analyses were also being reanalyzed with tracking,
-but it does not make much difference. 
-
-Last updated: 23 Aug 2007 by Kelly Fitz
+Last updated: 29 Aug 2007 by Kelly Fitz
 """
 
 import loris, time, os
@@ -32,6 +24,16 @@ tag = ''
 stuff = {}
 
 # ----------------------------------------------------------------------------
+
+"""
+The fried vocal ("nonsense") definitely gives better harmonic tracking when 
+reanalyzed using the tracking analysis and the fundamental extracted in the 
+first analysis, BUT the reconstruction is a little bit worse in that case. 
+
+Two partials per harmonic are always necessary for decent
+sound, and the sound doesn't improve with the improved tracking.
+One partial per harmonic is completely unlistenable.
+"""
 
 def doNonsense( exportDir = '' ):
 
@@ -107,6 +109,11 @@ def doNonsense( exportDir = '' ):
 
 
 # ----------------------------------------------------------------------------
+
+"""
+The "funny people" analyses were also being reanalyzed with tracking,
+but it does not make much difference. 
+"""
 
 def do_funnyPeeple1( exportDir = '' ):
 
@@ -201,7 +208,78 @@ def do_funnyPeeple2( exportDir = '' ):
 		
 	stuff[ name ] = ( p, ref, anal )
 	
+	print 'Done. (%s)'%(time.ctime(time.time()))	
+	
+# ----------------------------------------------------------------------------
+
+"""
+The parameters we once liked for this are resolution 90 and
+window width 200 Hz. More recently, 70 or 80 Hz resolution 
+have sounded better.
+
+Found that crunchiness is made _worse_ by increasing Distiller 
+fade time. With the default (1 ms), distilled sounds about the 
+same as raw.
+
+Tracking analysis makes little difference.
+"""
+
+
+def do_moses( exportDir = '' ):
+
+	name = 'moses'
+	f = loris.AiffFile( name + '.aiff' )
+	
+	print 'analyzing %s (%s)'%(name, time.ctime(time.time()))
+	anal = loris.Analyzer( 80, 200 )
+	# anal.setBwRegionWidth( 0 ) # no BW association
+	anal.buildFundamentalEnv( 90, 150 )
+	
+	p = anal.analyze( f.samples(), f.sampleRate() )
+	ref = anal.fundamentalEnv()
+
+	
+	# distill at N Partials per harmonic
+	print 'distilling %i partials (%s)'%(p.size(), time.ctime(time.time()))
+	N = 1
+	loris.channelize( p, ref, N )
+	loris.sift( p )
+	loris.removeLabeled( p, 0 )
+	loris.distill( p, 0.001 )
+	# loris.crop( p, 0, 10 )	# no longer needed as of Loris 1.5
+	
+
+# 	print 'performing tracking analysis'
+# 	ptk = anal.analyze( f.samples(), f.sampleRate(), ref )
+# 
+# 	# distill
+# 	print 'distilling %i partials (%s)'%(p.size(), time.ctime(time.time()))
+# 	loris.channelize( ptk, ref, 1 )
+# 	loris.sift( ptk )
+# 	loris.removeLabeled( ptk, 0 )
+# 	loris.distill( ptk )
+	
+	if exportDir:
+	
+		print 'synthesizing %i distilled partials (%s)'%(p.size(), time.ctime(time.time()))
+		out_sfile = loris.AiffFile( p, orate )
+		
+		opath = os.path.join( exportDir, name + tag + '.recon.aiff' ) 
+		print 'writing %s (%s)'%(opath, time.ctime(time.time()))
+		out_sfile.setMarkers( f.markers() )
+		out_sfile.write( opath )
+		
+		opath = os.path.join( exportDir, name + tag + '.sdif' )
+		print 'writing %s (%s)'%(opath, time.ctime(time.time()))
+		out_pfile = loris.SdifFile( p )
+		out_pfile.setMarkers( f.markers() )
+		out_pfile.write( opath )
+		
+		
+	stuff[ name ] = ( p, ref, anal )
+	
 	print 'Done. (%s)'%(time.ctime(time.time()))			
+	
 
 # ----------------------------------------------------------------------------
 
@@ -218,3 +296,5 @@ if __name__ == '__main__':
 	doNonsense( odir )
 	do_funnyPeeple1( odir )
 	do_funnyPeeple2( odir )
+	do_moses( odir )
+	
