670fd23b637b008031c0ad793d702e4d5207cafb
Kelly Fitz <kfitz@cerlsoundgroup.org>
2010-08-11 05:10:22 +0000

    ooops, moved Loris folder instead of copying it when tagging the release, fixing

--- jittery/m2pi.m
+++
@@ -1,9 +0,0 @@
-function y = m2pi(x)
-% Wrap phase into the range (-pi,pi].
-% -------------------------------------
-% y = m2pi(x)
-% x = unwrapped phase
-% y = wrapped phase
-%
-y = x+2*pi*round(-x/(2*pi));
-endfunction
